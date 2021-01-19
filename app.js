29135eb00f3468b269b7186838663ee366537c1a
黄玉才 <yc_huang97@163.com>
2021-01-19 09:21:16 +0800

    博客迁移测试

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-/* Registering Service Worker */ if('serviceWorker' in navigator) { navigator.serviceWorker.register('/HYCBlog/sw.js'); };
+/* Registering Service Worker */ if('serviceWorker' in navigator) { navigator.serviceWorker.register('/sw.js'); };
